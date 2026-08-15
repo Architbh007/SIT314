0afaac236145db30ff386e4d0a207fbe91c2c56b
Architbh007 <bhullararchit@gmail.com>
2026-08-15 17:25:55 +1000

    task 6c Light ON/OFF onfig

---
+++ Pass Tasks/week_6/iot_client.js
@@ -0,0 +1,21 @@
+let request = require('request');
+
+let url1 = `http://localhost:3001/lightOn`;
+let url2 = `http://localhost:3001/lightOff`;
+
+request(url1, function (err, response, body) {
+    if (err) {
+        console.log('error:', err);
+    } else {
+        console.log('Response 1:', body);
+    }
+});
+
+request(url2, function (err, response, body) {
+    if (err) {
+        console.log('error:', err);
+    } else {
+        console.log('Response 2:', body);
+    }
+});
+
