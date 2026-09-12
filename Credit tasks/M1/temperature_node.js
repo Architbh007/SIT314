853ec373a49a94aa27c59cb6db885ce07e8aa058
Architbh007 <bhullararchit@gmail.com>
2026-09-12 15:26:36 +1000

    individual sensor nodes genrating stimulated sensor readings

---
+++ Credit tasks/M1/temperature_node.js
@@ -0,0 +1,26 @@
+const net = require("net");
+const { generateTemp } = require("./climate_model");
+
+const host = "127.0.0.1";
+const port = 6000;
+const area = process.argv[2] || "Central";
+
+const client = net.createConnection(port, host, () => {
+    console.log("Connected");
+    setInterval(() => {
+        const temp = generateTemp(area, new Date());
+        client.write(`temp,${area},${temp}\n`);
+    }, 2000);
+});
+
+client.on("data", (data) => {
+    console.log(`Received: ${data}`);
+});
+
+client.on("error", (error) => {
+    console.log(`Error: ${error.message}`);
+});
+
+client.on("close", () => {
+    console.log("Connection closed");
+});
